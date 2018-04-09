fa8bdc0dcf4953ed26786e35dd46c9da919cf952
fjc0k <fjc0kb@gmail.com>
2018-04-09 18:49:38 +0800

    fix: update url of QR code

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export default {
     getMediaInfo: 'http://weixiao.qq.com/apps/v1/data/media-info',
     getMediaInfoJSONP: 'https://weixiao.qq.com/apps/v1/data/media-infop',
     getMediaKeywords: 'http://weixiao.qq.com/apps/v1/data/media-keywords',
-    getQRCodeImage: 'https://open.weixin.qq.com/qr/code/?username='
+    getQRCodeImage: 'https://open.weixin.qq.com/qr/code?username='
   }
 }
